11a790129849e5d7b59378304c07a8043bedd764
Rigo-rios <146138438+Rigo-rios@users.noreply.github.com>
2023-12-11 23:31:16 -0500

    added the test, still ugly code

--- InsClaims.Tests/UnitTest1.cs
+++ InsClaims.Tests/UnitTest1.cs
@@ -1,10 +1,28 @@
+using InsClaims.data;
+using InsClaims.Repository;
+
 namespace InsClaims.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    private ClaimsRepository _ClaimRepo;
+    public UnitTest1()
     {
-        
+        _ClaimRepo = new ClaimsRepository();
+    }
+    
+
+
+
+    [Fact]
+    public void GetClaimsFiled_Have4Claims()
+    {
+        // Arrange
+        Queue<ClaimFiled> _claimsInDb = _ClaimRepo.GetClaimsFiled();
+        // Act
+        int expectedCount = 4;
+        int actualCount = _claimsInDb.Count();
+        //Assert
+        Assert.Equal(expectedCount,actualCount);
     }
 }
